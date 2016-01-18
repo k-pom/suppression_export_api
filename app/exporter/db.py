fed8d2f6ec6581da3d09bced97f167c52f918e30
Kaleb Pomeroy <kaleb@seatsmart.com>
2016-01-18 10:36:17 -0600

    Remove /app for PR

--- app/exporter/db.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-import os
-
-client = MongoClient(os.environ["DB_HOST"])
-client.suppressions.authenticate(os.environ["DB_USER"], os.environ["DB_PASS"])
-
-db = client.suppressions
-exports = db.exports
--- app/js/actions/router.js
+++
@@ -1,9 +0,0 @@
-
-export const SET_PATH = 'SET_PATH';
-
-
-export function setPath(path) {
-  return (dispatch) => {
-    dispatch({type: SET_PATH, path: path});
-  };
-}
